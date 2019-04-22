e09eb341890035d6405213fbb14b461a664fff17
Ptaq <antyplastik@gmail.com>
2019-04-22 17:45:30 +0200

    spring configuration

---
+++ src/main/java/com/pl/ptaq/projectManager/configuration/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.pl.ptaq.projectManager.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/logout").setViewName("logout");
+    }
+
+}
---
+++ src/main/java/com/pl/ptaq/projectManager/configuration/UserPage.java
@@ -0,0 +1,7 @@
+package com.pl.ptaq.projectManager.configuration;
+
+import org.springframework.web.bind.annotation.RestController;
+
+public class UserPage {
+
+}
